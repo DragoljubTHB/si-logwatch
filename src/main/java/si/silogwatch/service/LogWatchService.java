0941dad005a8ab4252f8ee6effd1f7f99af3f367
DragoljubTHB <littlevwing@live.com>
2018-01-20 09:20:47 +0100

    using for service spring-Transactional

---
+++ src/main/java/si/silogwatch/service/LogWatchService.java
@@ -0,0 +1,10 @@
+package si.silogwatch.service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Named;
+
+@Named
+@Transactional(rollbackFor = Exception.class)
+public class LogWatchService {
+}
