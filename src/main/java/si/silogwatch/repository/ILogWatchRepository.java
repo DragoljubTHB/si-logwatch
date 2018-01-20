5d8f73f33f4d3f07a979db2bee980d05b5515ea8
DragoljubTHB <littlevwing@live.com>
2018-01-20 09:18:48 +0100

    embedded ILogWatchRepo in spring Repo

--- src/main/java/si/silogwatch/repository/ILogWatchRepository.java
+++ src/main/java/si/silogwatch/repository/ILogWatchRepository.java
@@ -1,7 +1,12 @@
 package si.silogwatch.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import si.silogwatch.model.LogWatch;
+
 import javax.inject.Named;
+import java.util.UUID;
 
 @Named
-public interface ILogWatchRepository {
+public interface ILogWatchRepository extends JpaRepository<LogWatch, UUID>, JpaSpecificationExecutor<LogWatch>{
 }
