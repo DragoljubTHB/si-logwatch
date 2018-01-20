95a86acfa834aacd6ec288187fa57dbe48dfcf39
DragoljubTHB <littlevwing@live.com>
2018-01-20 08:20:20 +0100

    logger watcher and communicator via websockets

---
+++ src/main/java/si/silogwatch/SiLogwatchApplication.java
@@ -0,0 +1,12 @@
+package si.silogwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiLogwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiLogwatchApplication.class, args);
+	}
+}
