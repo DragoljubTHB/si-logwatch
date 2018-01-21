53dab88cd99d7c03242e6c8429ae5f673017fb09
DragoljubTHB <littlevwing@live.com>
2018-01-21 17:46:32 +0100

    minor

--- src/test/java/si/silogwatch/rest/ITBase.java
+++ src/test/java/si/silogwatch/rest/ITBase.java
@@ -1,5 +1,6 @@
 package si.silogwatch.rest;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -21,5 +22,10 @@ public class ITBase {
 
     public void cleanup() {
         System.out.println("clean up");
+    }
+
+    @Test
+    public void show_port() {
+        System.out.println(port);
     }
 }
